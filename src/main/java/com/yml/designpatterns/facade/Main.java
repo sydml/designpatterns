9ddfcc2e0c77fb7981db92c7010cceb55fbce506
Liuym <liuym@startimes.com.cn>
2018-12-29 17:32:23 +0800

    对观察者模式，门面模式增加注解

--- src/main/java/com/yml/designpatterns/facade/Main.java
+++ src/main/java/com/yml/designpatterns/facade/Main.java
@@ -1,6 +1,8 @@
 package com.yml.designpatterns.facade;
 
 /**
+ * 门面模式：1.简化底层api的调用，2：隐藏不必要的返回结果
+ *
  * @author Liuym
  * @date 2018/12/25 0025
  */
--- src/main/java/com/yml/designpatterns/observer/Main.java
+++ src/main/java/com/yml/designpatterns/observer/Main.java
@@ -2,6 +2,7 @@ package com.yml.designpatterns.observer;
 
 /**
  * 生产者更新了状态，通知所有的消费者状态发生了变化
+ * 好处：遵循了开闭原则
  *
  * @author Liuym
  * @date 2018/12/25 0025
