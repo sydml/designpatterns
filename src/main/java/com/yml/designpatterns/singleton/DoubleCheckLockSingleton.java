6a0b192e6324e358e3bb831a113ca0b91464a7f9
liuyuming11 <liuyuming11@jd.com>
2020-12-04 10:07:39 +0800

    注释修改

--- src/main/java/com/yml/designpatterns/singleton/DoubleCheckLockSingleton.java
+++ src/main/java/com/yml/designpatterns/singleton/DoubleCheckLockSingleton.java
@@ -2,6 +2,7 @@ package com.yml.designpatterns.singleton;
 
 /**
  * 双检锁单利模式，解决并发问题，volatile 解决指令重排序导致对象内部变量引用对象未实例化完成出现问题
+ * 第二层检查的目的是为了多线程时创建多个实例问题
  * @author Liuym
  * @date 2018/12/29 0029
  */
